eb1938e448f5c9e21a9c37eba81ea4d4a57ac5fc
feragst <139084664+feragst@users.noreply.github.com>
2024-06-03 21:22:37 -0300

    Update-projeto

--- TimeFutebol/src/models/Jogador.java
+++ TimeFutebol/src/models/Jogador.java
@@ -9,5 +9,79 @@ package models;
  * @author ferag
  */
 public class Jogador {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNrCamisa() {
+        return nrCamisa;
+    }
+
+    public void setNrCamisa(int nrCamisa) {
+        this.nrCamisa = nrCamisa;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+    private int id;
+    private String nome;
+    private int nrCamisa;
+    private int idade;
+    private String posicao;
+    
+    
+     public Jogador(int id, String nome, int nrCamisa,  int idade, String posicao) {
+        this.id = id;
+        this.nome = nome;
+        this.nrCamisa = nrCamisa;
+        this.idade = idade;
+        this.posicao = posicao;
+        
+    }
+     public Jogador( String nome, int nrCamisa,  int idade, String posicao) {
+        this.nome = nome;
+        this.nrCamisa = nrCamisa;
+        this.idade = idade;
+        this.posicao = posicao;
+        
+    }
+     public Jogador (){
+         
+     }
+      public void printAttributes() {
+        System.out.println("ID: " + id);
+        System.out.println("Nome: " + nome);
+        System.out.println("Numero da camisa: " + nrCamisa);
+        System.out.println("Idade: " + idade);
+        System.out.println("Posicao: " + posicao);
+        
+    }
+     
     
 }
--- TimeFutebol/src/models/Time.java
+++ TimeFutebol/src/models/Time.java
@@ -9,5 +9,72 @@ package models;
  * @author ferag
  */
 public class Time {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoFundacao() {
+        return anoFundacao;
+    }
+
+    public void setAnoFundacao(int anoFundacao) {
+        this.anoFundacao = anoFundacao;
+    }
+
+    public int getQtdTitulos() {
+        return qtdTitulos;
+    }
+
+    public void setQtdTitulos(int qtdTitulos) {
+        this.qtdTitulos = qtdTitulos;
+    }
+
+    public int getDivisao() {
+        return divisao;
+    }
+
+    public void setDivisao(int divisao) {
+        this.divisao = divisao;
+    }
+    private int id;
+    private String nome;
+    private int anoFundacao;
+    private int qtdTitulos;
+    private int divisao;
+    
+    public Time (int id, String nome, int anoFundacao,  int qtdTitulos, int divisao){
+        this.id = id;
+        this.nome = nome;
+        this.anoFundacao = anoFundacao;
+        this.qtdTitulos = qtdTitulos;
+        this.divisao = divisao;
+    }
+     public Time (String nome, int anoFundacao,  int qtdTitulos, int divisao){
+        this.nome = nome;
+        this.anoFundacao = anoFundacao;
+        this.qtdTitulos = qtdTitulos;
+        this.divisao = divisao;
+    }
+       public void printAttributes() {
+        System.out.println("ID: " + id);
+        System.out.println("Nome do time: " + nome);
+        System.out.println("Ano de fundacao: " + anoFundacao);
+        System.out.println("Quantidade de titulos: " + qtdTitulos);
+        System.out.println("Divisao: " + divisao);
+       }
+    
     
 }
